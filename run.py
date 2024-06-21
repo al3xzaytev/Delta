d2967e31197bf11863199583f0d74b5a7a6398ce
al3xzaytev <ieatsyrniks@gmail.com>
2024-06-21 23:34:19 +0700

    Modify how creating new player replacements and player stat reset works
    - Creating new player replacements now only happens when a group is defeated.
    - If a group defeats a monster, but has dead players, they will be revived and stats set to default at the start of next level.

--- run.py
+++ run.py
@@ -27,40 +27,52 @@ def start_level(player_count):
     player_storage = []
 
     while True:
-        # Logically, this should only be run once at the beginning of every level...
+        # Logically, this should only run once at the beginning of every level...
         if len(player_list) < player_count:
             for i in range(player_count):
                 player = monsters.initialize("player")
                 player_storage.append(copy.deepcopy(player))
                 player_list.update({player: [player.name, "Alive"]})
 
-        # Remove players from player_storage if all players are dead
+        # Remove players from player_storage if all players are dead (i.e. level restart)
         if not new_level:
             player_storage.clear()
 
-        # Initializes new players in place of dead ones
-        for players, info in player_list.copy().items():
-            if info[1] == "Dead":
-                new_player = monsters.initialize("player")
-                player_storage.append(copy.deepcopy(new_player))
-                player_list[new_player] = player_list.pop(players)
-                player_list[new_player] = [new_player.name, "Alive"]
-
-        # Convoluted player stat reset after finishing a level
-        z = 0
-        while z < player_count:
-            for player_object in player_list.keys():
-                if player_object.name == player_storage[z].name:
-                    if not monsters.compare_with_base(player_storage[z], player_object):
-                        replacement = copy.deepcopy(player_storage[z])
-                        player_list[replacement] = player_list.pop(player_object)
-                        z += 1
-                        break
-                    else:
-                        z += 1
-                        continue
+            # Initializes new players in place of dead ones in level restart
+            for players, info in player_list.copy().items():
+                if info[1] == "Dead":
+                    new_player = monsters.initialize("player")
+                    player_storage.append(copy.deepcopy(new_player))
+                    player_list[new_player] = player_list.pop(players)
+                    player_list[new_player] = [new_player.name, "Alive"]
+                    continue
                 else:
                     continue
+
+        # If remaining teammates of the group wins (and moves on to new level),
+        # dead players should be revived and original stats brought back.
+        elif new_level:
+            for players, info in player_list.copy().items():
+                if info[1] == "Dead":
+                    player_list[players] = [players.name, "Alive"]
+                    continue
+                else:
+                    continue
+            # Convoluted player stat reset after finishing a level
+            z = 0
+            while z < player_count:
+                for player_object in player_list.keys():
+                    if player_object.name == player_storage[z].name:
+                        if not monsters.compare_with_base(player_storage[z], player_object):
+                            replacement = copy.deepcopy(player_storage[z])
+                            player_list[replacement] = player_list.pop(player_object)
+                            z += 1
+                            break
+                        else:
+                            z += 1
+                            continue
+                    else:
+                        continue
 
         if new_level:
             print("================================================================================")
